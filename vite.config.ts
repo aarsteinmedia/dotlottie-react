9d146871b9737a27442d7fab543f95cb4481a11f
Johan Martin Aarstein <johan@aarstein.media>
2025-06-18 13:04:51 +0200

    stash

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import react from '@vitejs/plugin-react-swc'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -15,8 +17,21 @@ export default defineConfig({
       ),
       formats: ['es']
     },
+    rollupOptions: {
+      external: [
+        '@aarsteinmedia/lottie-web',
+        '@aarsteinmedia/lottie-web/utils',
+        'fflate',
+        'react',
+        'react/jsx-runtime'
+      ]
+    },
     target: 'ES2023'
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ['lib'] })
+  ],
   resolve: { alias: { '@': resolve(__dirname, 'lib') } },
 })
