f8a4cdbedd3fe084a9a255b2cf59062a55cfbf73
Johan Martin Aarstein <johan@aarstein.media>
2025-06-19 15:38:46 +0200

    0.1.4

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import preserveDirectives from 'rollup-preserve-directives'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import { libInjectCss } from 'vite-plugin-lib-inject-css'
@@ -30,6 +31,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    preserveDirectives(),
     libInjectCss(),
     dts({
       include: ['lib'],
